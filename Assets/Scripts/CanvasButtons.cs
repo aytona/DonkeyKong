00842f3a6f5a471412c72b36e2da65caaaf6eaf7
aytona <christopher.aytona@gmail.com>
2015-06-29 14:07:38 -0400

    Canvas Buttons

---
+++ Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasButtons : MonoBehaviour {
+	public void StartGame() {
+		Application.LoadLevel ("Level1");
+	}
+
+	public void MainMenu() {
+		Application.LoadLevel ("Menu");
+	}
+
+	public void HowTo() {
+		Application.LoadLevel ("HowTo");
+	}
+
+	public void Quit() {
+		Application.Quit ();
+	}
+}
