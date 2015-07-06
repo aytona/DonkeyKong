d7c14d625f58514aa74b9014233b88bbf9df4b5b
aytona <christopher.aytona@gmail.com>
2015-07-06 14:24:15 -0400

    Added Mario's Walking Ability

--- Assets/Scripts/JumpmanControls.cs
+++ Assets/Scripts/JumpmanControls.cs
@@ -3,26 +3,13 @@ using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class JumpmanControls : MonoBehaviour {
-
-    // WHY CHANGE RIGIDBODY
     Rigidbody2D rb2d;
-
-    private Animator animator = null;
-    private GameObject spriteContainer = null;
-    private Transform groundCheck = null;
-
-    // Animation states
-    private bool onGround = false;
-    private bool hasHammer = false;
-    private bool onLadder = false;
-    private bool jump = false;
-    private bool death = false;
-
-    // Movement variables
+    [SerializeField]private Animator animator = null;
+    [SerializeField]private GameObject spriteContainer = null;
+    
     private Vector2 direction = Vector2.zero;
     private float walkForce = 10f;
-    private float maxWalkSpeed = 4f; 
-    private float jumpForce = 1f;
+    private float maxWalkSpeed = 4f;
 
     void Awake()
     {
@@ -31,118 +18,63 @@ public class JumpmanControls : MonoBehaviour {
 
     void FixedUpdate()
     {
-        OrientCharacter(this.direction);
         Walk(this.direction);
-        Jump(this.direction);
     }
 
     void Update()
     {
         ProcessInput();
-        CheckGround();
-        CheckLadder();
     }
 
     private void ProcessInput()
     {
         ProcessWalking();
-        ProcessJump();
-        ProcessClimbing();
     }
 
-    // NOTE: Only when groundcheck is true
+    // Changes the x vector depending on the keys pressed
     private void ProcessWalking()
     {
         this.direction = Vector2.zero;
-        if (onGround == true)
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                this.direction += new Vector2(-1, 0);
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                this.direction += new Vector2(1, 0);
-            }
+            this.direction += new Vector2(-1, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.direction += new Vector2(1, 0);
         }
     }
 
-    // NOTE: Only when groundcheck is true
-    private void ProcessJump()
-    {
-        // Only be able to jump while groundcheck is true
-        if (Input.GetKeyDown(KeyCode.Space) && onGround == true)
-        {
-            this.direction.y = 1;
-        }
-    }
-
-    // Note: Check ladder is true
-    private void ProcessClimbing()
-    {
-        // Only be able to climb while on ladder
-        this.direction = Vector2.zero;
-        if (onLadder == true)
-        {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                this.direction += new Vector2(0, 1);
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                this.direction += new Vector2(0, -1);
-            }
-        }
-    }
-
+    // Orients the character to the proper direction they are walking towards to
     private void OrientCharacter(Vector2 direction)
     {
         Vector3 spriteScale = this.spriteContainer.transform.localScale;
+        // Movement direction towards the right
         if (direction.x > 0)
         {
-            spriteScale.x = -3;
+            spriteScale.x = 3;
         }
+        // Movement direction towards the left
         else if (direction.x < 0)
         {
-            spriteScale.x = 3;
+            spriteScale.x = -3;
         }
         this.spriteContainer.transform.localScale = spriteScale;
     }
 
+    // Walking function
     private void Walk(Vector2 direction)
     {
         this.rb2d.AddForce(direction * this.walkForce);
         float horizontalSpeed = Mathf.Abs(this.rb2d.velocity.x);
         if (Mathf.Abs(horizontalSpeed) > this.maxWalkSpeed)
         {
+
             Vector2 newVelocity = this.rb2d.velocity;
             float multiplier = (this.rb2d.velocity.x > 0) ? 1 : -1;
             newVelocity.x = multiplier * maxWalkSpeed;
             this.rb2d.velocity = newVelocity;
         }
-        // TODO: Animator settings
-        // this.animator.settrigger("walking");
-    }
-
-    private void Jump(Vector2 direction)
-    {
-        if (direction.y > 0)
-        {
-            this.rb2d.AddForce(Vector2.up * this.jumpForce);
-            direction.y = 0;
-            // TODO: Animator settings
-            // this.animator.settrigger("Jumping");
-        }
-    }
-
-    private void Climb(Vector2 direction)
-    {
-
-    }
-
-    private void CheckGround()
-    {
-        Collider2D collider = Physics2D.OverlapPoint(this.groundCheck.transform.position);
-        this.onGround = (collider != null);
+        this.animator.SetTrigger("Walking");
     }
 }
