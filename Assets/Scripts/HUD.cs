181ad34fb1b3d798563ad31c3a9b5eff84b337a2
aytona <christopher.aytona@gmail.com>
2015-07-08 23:24:32 -0400

    Screen Functionality Added

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.LoadLevel("Menu");
+        }
+	}
+}
