1d8098f51a01ddfff7f4ae1485eb9bf7b8b4670e
aytona <christopher.aytona@gmail.com>
2015-08-10 20:06:19 -0400

    Intro Audio Completed

---
+++ Assets/Scripts/IntroAudio.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroAudio : MonoBehaviour {
+
+    private static IntroAudio instance = null;
+    public static IntroAudio Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void OnLevelWasLoaded()
+    {
+        if (Application.loadedLevelName == "Level1")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
