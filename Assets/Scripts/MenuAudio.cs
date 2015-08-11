b1e6b6e14baf9fe0c5697d6b921acb9e73e72295
aytona <christopher.aytona@gmail.com>
2015-08-10 21:53:27 -0400

    Menu Background Music Addded

---
+++ Assets/Scripts/MenuAudio.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAudio : MonoBehaviour {
+
+    private static MenuAudio instance = null;
+    public static MenuAudio Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void OnLevelWasLoaded()
+    {
+        if (Application.loadedLevelName == "Intro")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
