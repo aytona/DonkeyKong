56c99a6dba0737c471893d8779586196ee87917c
aytona <christopher.aytona@gmail.com>
2015-08-13 13:10:18 -0400

    First Fire Sprite Added

--- Assets/Scripts/BarrelKeyEvents.cs
+++ Assets/Scripts/BarrelKeyEvents.cs
@@ -1,10 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BarrelKeyEvents : MonoBehaviour {
+
+    List<GameObject> fireList = new List<GameObject>();
+
+    public GameObject fire1;
+
+    void Start()
+    {
+        fireList.Add(fire1);
+    }
 
     void DestroyThis()
     {
         Destroy(this.gameObject);
+    }
+
+    void SpawnFire()
+    {
+        int fireIndex = UnityEngine.Random.Range(0, fireList.Count);
+        Instantiate(fireList[fireIndex]);
     }
 }
